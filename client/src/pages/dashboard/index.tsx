2edf064d9a2a83cb2dc98c5b917c3b6732eb6c5a
Christian <Christianmnaira@gmail.com>
2026-02-05 01:51:16 +0800

    feat: added a form

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,4 +1,6 @@
 import { useUser } from "@clerk/clerk-react";
+import { FinancialRecordForm } from "./financial-record-form";
+import { FinancialRecordList } from "./financial-record-list";
 
 export const Dashboard = () => {
     const {user} = useUser();
@@ -6,6 +8,8 @@ export const Dashboard = () => {
     return (
         <div className="dashboard-container">
             <h1>Welcome {user?.firstName}! Here Are Your Finances:</h1>
+            <FinancialRecordForm/>
+            <FinancialRecordList/>
         </div>
     )
 }
