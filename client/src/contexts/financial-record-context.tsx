1e8792cee4655d5abdae8778042995a8117b291f
Christian <Christianmnaira@gmail.com>
2026-02-06 09:24:38 +0800

    fix: added headers to addRecord functionality

--- client/src/contexts/financial-record-context.tsx
+++ client/src/contexts/financial-record-context.tsx
@@ -29,9 +29,12 @@ export const FinancialRecordsProvider = ({
     const [records, setRecords] = useState<FinancialRecord[]>([]);
 
     const addRecord = async (record: FinancialRecord) => {
-        const response = await fetch("https://localhost:3001/financial-records", {
+        const response = await fetch("http://localhost:3001/financial-records", {
             method: "POST",
             body: JSON.stringify(record),
+            headers: {
+                "Content-Type": "application/json",
+            },
         });
 
         try {
